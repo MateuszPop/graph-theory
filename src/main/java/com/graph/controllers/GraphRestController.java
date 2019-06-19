4f950c7a58a5ec06f68da0d3140352e77db9bc54
Mateusz Popławski <ajoga1995@gmail.com>
2019-06-19 23:35:00 +0200

    Correct typo

--- src/main/java/com/graph/controllers/GraphRestController.java
+++ src/main/java/com/graph/controllers/GraphRestController.java
@@ -17,7 +17,7 @@ import org.springframework.web.multipart.MultipartFile;
 import java.io.IOException;
 
 @RestController
-public class GrafyRestController {
+public class GraphRestController {
 
     private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
